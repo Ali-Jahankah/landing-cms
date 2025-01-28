786fc69e9b71ff90b0644aafc04caf552b575b9d
Ali Jahankah <aral2@yahoo.com>
2025-01-28 19:12:28 +0000

    fix: refactore sublinks

--- src/app/components/main/DesktopNav.jsx
+++ src/app/components/main/DesktopNav.jsx
@@ -1,5 +1,6 @@
 import { navBarConfig } from '@/app/config/navbar';
 import Link from 'next/link';
+import DesktopSubLink from '../sub/DesktopSubLink';
 
 const DesktopNav = () => {
   return (
@@ -18,15 +19,7 @@ const DesktopNav = () => {
               className="absolute left-0 top-14 w-max h-40 overflow-auto transform origin-top scale-y-0 group-hover:scale-y-100 transition-all duration-300 ease-out shadow-xl bg-buttonBackground gap-2 flex flex-wrap flex-col py-3"
               style={{ transformOrigin: 'top' }}
             >
-              {linkOption.group.map((subLink, index) => (
-                <Link
-                  key={index}
-                  href="#"
-                  className="block py-2 px-6 hover:bg-lightBlue hover:text-buttonBackground transition-all duration-300"
-                >
-                  {subLink.label}
-                </Link>
-              ))}
+              <DesktopSubLink group={linkOption.group} />
             </div>
           </div>
         ) : (

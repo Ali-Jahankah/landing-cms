662d6dee5a4a70e8764f4fcda2ccbc04c113026f
Ali Jahankah <aral2@yahoo.com>
2025-01-30 22:55:46 +0000

    feat: set up contentful

---
+++ src/app/utils/contentful.js
@@ -0,0 +1,22 @@
+import { createClient } from 'contentful';
+
+const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+
+if (!space || !accessToken) {
+  throw new Error(
+    'Missing Contentful environment variables. Please set CONTENTFUL_SPACE_ID and CONTENTFUL_ACCESS_TOKEN.'
+  );
+}
+let client;
+
+try {
+  client = createClient({
+    space,
+    accessToken
+  });
+} catch (error) {
+  throw new Error('Contentful client initialization failed.');
+}
+
+export default client;

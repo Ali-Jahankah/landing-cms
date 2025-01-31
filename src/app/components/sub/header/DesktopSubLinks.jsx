5148f2a43cbbc57d19f945a42c2a13784a388260
Ali Jahankah <aral2@yahoo.com>
2025-01-31 01:42:35 +0000

    fix: refactore and seo

--- src/app/components/sub/header/DesktopSubLinks.jsx
+++ src/app/components/sub/header/DesktopSubLinks.jsx
@@ -3,16 +3,17 @@ import Link from 'next/link';
 const DesktopSubLinks = ({ group }) => {
   return (
     <div
-      className="absolute left-0 top-14 w-max h-40 overflow-auto transform origin-top scale-y-0 group-hover:scale-y-100 transition-all duration-300 ease-out shadow-xl bg-buttonBackground gap-2 flex flex-wrap flex-col py-3"
+      className={`absolute left-0 top-14 w-max min-w-[400px] h-52 overflow-auto transform origin-top scale-y-0 group-hover:scale-y-100 transition-all duration-300 ease-out shadow-xl bg-buttonBackground gap-2 flex flex-wrap flex-col py-3`}
       style={{ transformOrigin: 'top' }}
     >
-      {group.map((subLink, index) => (
+      {group.map((subLink) => (
         <Link
-          key={index}
-          href="#"
-          className="block py-2 px-6 hover:bg-lightBlue hover:text-buttonBackground transition-all duration-300"
+          key={subLink?.label}
+          href={subLink?.href}
+          className="block font-medium py-2 px-6 hover:bg-lightBlue hover:text-buttonBackground transition-all duration-300"
+          aria-label={`Navigate to ${subLink?.label}`}
         >
-          {subLink.label}
+          {subLink?.label}
         </Link>
       ))}
     </div>

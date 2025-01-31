7044fa14dccd5f0fda1313799f2e6fef9e6549e7
Ali Jahankah <aral2@yahoo.com>
2025-01-31 01:42:53 +0000

    fix: pull data from contentful

--- src/app/components/sub/header/HeaderLogo.jsx
+++ src/app/components/sub/header/HeaderLogo.jsx
@@ -1,11 +1,34 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { fetchHeaderData } from '@/app/utils/healpers';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const HeaderLogo = () => {
+  const [headerData, setHeaderData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchHeaderData();
+      setHeaderData(data); // update the state with fetched data
+    };
+
+    fetchData();
+  }, []);
+  if (!headerData) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <Link href="/">
-        <Image src="/images/logo.png" alt="Logo" width={150} height={50} />
+        <Image
+          src={'https:' + headerData.headerLogo.url}
+          alt="Header logo"
+          width={100}
+          height={30}
+          className="w-auto"
+          priority={false}
+          quality={75}
+        />
       </Link>
     </div>
   );

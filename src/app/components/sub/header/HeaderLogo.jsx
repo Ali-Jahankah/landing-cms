90c2e693c6ff4a23cbce2f132bebc59d93c38e13
Ali Jahankah <aral2@yahoo.com>
2025-02-11 17:14:15 +0000

    Fix: change to server component

--- src/app/components/sub/header/HeaderLogo.jsx
+++ src/app/components/sub/header/HeaderLogo.jsx
@@ -1,33 +1,21 @@
-'use client';
-import { useState, useEffect } from 'react';
 import { fetchHeaderData } from '@/app/utils/healpers';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const HeaderLogo = () => {
-  const [headerData, setHeaderData] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchHeaderData();
-      setHeaderData(data); // update the state with fetched data
-    };
-
-    fetchData();
-  }, []);
-  if (!headerData) {
-    return <div>Loading...</div>;
-  }
+const HeaderLogo = async () => {
+  const data = await fetchHeaderData();
   return (
     <div>
       <Link href="/">
         <Image
-          src={'https:' + headerData.headerLogo.url}
+          src={'https:' + data.headerLogo.url}
           alt="Header logo"
           width={100}
           height={30}
           className="w-auto"
           priority={false}
           quality={75}
+          role="img"
         />
       </Link>
     </div>

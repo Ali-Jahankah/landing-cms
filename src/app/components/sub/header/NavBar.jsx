8af007ca3e1773dfbb712420262d95e58d18288d
Ali Jahankah <aral2@yahoo.com>
2025-02-11 16:59:52 +0000

    fix: make client component in a smaller component

--- src/app/components/sub/header/NavBar.jsx
+++ src/app/components/sub/header/NavBar.jsx
@@ -1,30 +1,16 @@
-'use client';
-import { useState } from 'react';
 import MobileNav from './MobileNav';
 import HeaderLogo from '../../sub/header/HeaderLogo';
-import BurgerButton from '../../sub/header/BurgerButton';
 import DesktopNav from './DesktopNav';
 
 const NavBar = () => {
-  const [navBarIsOpen, setNavBarIsOpen] = useState(false);
-
-  const toggleMenu = () => setNavBarIsOpen(!navBarIsOpen);
-
   return (
     <nav className="w-full px-4" aria-label="Main Navigation">
       <div className="flex justify-between items-center">
         <HeaderLogo />
         <DesktopNav />
-        <BurgerButton
-          toggleMenu={toggleMenu}
-          navBarIsOpen={navBarIsOpen}
-          aria-label="Toggle mobile menu"
-        />
+
+        <MobileNav />
       </div>
-      <MobileNav
-        setNavBarIsOpen={setNavBarIsOpen}
-        navBarIsOpen={navBarIsOpen}
-      />
     </nav>
   );
 };

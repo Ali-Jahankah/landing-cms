79750bbaa04ec5f0ec531671a561ef165e82d918
Ali Jahankah <aral2@yahoo.com>
2025-02-11 17:56:45 +0000

    fix: flex styles for sm size

--- src/app/components/sub/main-body/features/FeatureCard.jsx
+++ src/app/components/sub/main-body/features/FeatureCard.jsx
@@ -2,19 +2,21 @@ import Image from 'next/image';
 
 const FeatureCard = ({ card }) => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg border-y-lightBlue border-x-2 border-y-2 border-solid">
+    <div className="bg-white p-2 flex md:flex-col rounded-lg shadow-lg border-y-lightBlue border-x-2 border-y-2 border-solid">
       <Image
         src={'https:' + card.featureImage.fields.file.url}
         alt={card.featureTitle + ' iamge'}
         width={200}
         height={150}
-        className="w-full h-40 object-cover rounded-md"
+        className="w-5/12 md:w-full object-cover h-40 rounded-md"
         loading="lazy"
       />
-      <h1 className="mt-4 text-xl font-semibold text-black">
-        {card.featureTitle}
-      </h1>
-      <p className="mt-2 text-darkText">{card.featureDescription}</p>
+      <div className="flex flex-col px-2">
+        <h1 className="text-xl font-semibold text-black">
+          {card.featureTitle}
+        </h1>
+        <p className="mt-2 text-darkText">{card.featureDescription}</p>
+      </div>
     </div>
   );
 };
